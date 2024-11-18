49375f6890a6b0a8a0a15892d4b20b335657d444
gkkary3 <nambawon1@naver.com>
2024-11-18 22:46:40 +0900

    select Team Commit

--- src/App.js
+++ src/App.js
@@ -1,96 +1,10 @@
-import React, { useState } from 'react';
-import teamsData from './resource/data.js';
+import React from "react";
+import "./index.css"; // Tailwind CSS 파일
+
+import SelectTeam from "./Components/SelectTeam.jsx";
 
 function App() {
-  // 상태 변수
-  const [selectedTeam, setSelectedTeam] = useState('');
-  const [selectedName, setSelectedName] = useState('');
-  const [userOptions, setUserOptions] = useState([]);
-
-  // 팀 선택 시 해당 팀에 맞는 유저 목록을 업데이트
-  const handleTeamChange = (event) => {
-    const team = event.target.value;
-    setSelectedTeam(team);
-    setSelectedName(''); // 팀 변경 시 이전에 선택된 이름 초기화
-
-    // 선택된 팀에 맞는 사용자 목록을 업데이트
-    if (team && teamsData[team]) {
-      setUserOptions(Object.values(teamsData[team].users));
-    } else {
-      setUserOptions([]);
-    }
-  };
-
-  // 이름 선택
-  const handleNameChange = (event) => {
-    setSelectedName(event.target.value);
-  };
-
-  // 확인 버튼 클릭 시
-  const handleSubmit = () => {
-    if (!selectedTeam || !selectedName) {
-      alert('팀과 이름을 모두 선택해주세요.');
-    } else {
-      alert(`팀: ${teamsData[selectedTeam].name}, 이름: ${selectedName}`);
-    }
-  };
-
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
-      <div className="w-full max-w-md bg-gray-800 p-8 rounded-lg shadow-xl">
-        <h1 className="text-3xl font-semibold text-center text-blue-400 mb-6">주간 보고 프로젝트</h1>
-
-        {/* 팀 선택 */}
-        <div className="mb-4">
-          <label htmlFor="team" className="block text-lg font-medium text-gray-300">팀 선택</label>
-          <select
-            id="team"
-            name="team"
-            value={selectedTeam}
-            onChange={handleTeamChange}
-            className="w-full mt-2 p-2 border border-gray-600 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-          >
-            <option value="">팀을 선택하세요</option>
-            {Object.keys(teamsData).map((teamKey) => (
-              <option key={teamKey} value={teamKey}>
-                {teamsData[teamKey].name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* 이름 선택 */}
-        <div className="mb-6">
-          <label htmlFor="name" className="block text-lg font-medium text-gray-300">이름 선택</label>
-          <select
-            id="name"
-            name="name"
-            value={selectedName}
-            onChange={handleNameChange}
-            className="w-full mt-2 p-2 border border-gray-600 bg-gray-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            disabled={!selectedTeam}
-          >
-            <option value="">이름을 선택하세요</option>
-            {userOptions.map((user) => (
-              <option key={user.email} id={user.email} value={user.name}>
-                {user.name} ({user.email})
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* 확인 버튼 */}
-        <div className="flex justify-center">
-          <button
-            onClick={handleSubmit}
-            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-          >
-            확인
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <SelectTeam />;
 }
 
 export default App;
---
+++ src/Components/SelectTeam.jsx
@@ -0,0 +1,243 @@
+import React, { useEffect, useRef, useState } from "react";
+import teamsData from "../resource/data.js";
+import Modal from "./Modal.jsx";
+import { v4 as uuidv4 } from "uuid";
+
+export default function SelectTeam() {
+  const [selectedTeam, setSelectedTeam] = useState("");
+  const [selectedName, setSelectedName] = useState("");
+  const [userOptions, setUserOptions] = useState([]);
+  const [teamData, setTeamData] = useState({ ...teamsData });
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalType, setModalType] = useState(""); // 모달 타입: "team" or "user"
+
+  const assignTeam = useRef();
+  const assignUserName = useRef();
+  const assignUserEmail = useRef();
+
+  useEffect(() => {
+    if (selectedTeam && teamData[selectedTeam]) {
+      setUserOptions(Object.values(teamData[selectedTeam].users));
+    } else {
+      setUserOptions([]);
+    }
+  }, [teamData, selectedTeam]); // teamData나 selectedTeam이 변경될 때마다 실행
+
+  const handleNameChange = (event) => {
+    setSelectedName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!selectedTeam || !selectedName) {
+      alert("팀과 이름을 모두 선택해주세요.");
+    } else {
+      alert(`팀: ${teamData[selectedTeam].name}, 이름: ${selectedName}`);
+    }
+  };
+
+  const handleTeamChange = (event) => {
+    const team = event.target.value;
+    setSelectedTeam(team);
+    setSelectedName("");
+    if (team && teamData[team]) {
+      setUserOptions(Object.values(teamData[team].users));
+    } else {
+      setUserOptions([]);
+    }
+  };
+
+  const handleOpenModal = (type) => {
+    setModalType(type);
+    setModalIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+    setModalType("");
+  };
+
+  const handleAddTeam = () => {
+    var teamName = assignTeam.current.value;
+    const newTeamKey = `team-${uuidv4()}`;
+    // setTeamData((teamData[newTeamKey] = { name: teamName, users: {} }));
+    setTeamData((prevData) => ({
+      ...prevData, // 기존 데이터를 유지
+      [newTeamKey]: { name: teamName, users: {} }, // 새로운 팀 추가
+    }));
+    handleCloseModal();
+  };
+
+  const handleAddUser = () => {
+    const userName = assignUserName.current.value;
+    const userEmail = assignUserEmail.current.value;
+
+    if (!userName.trim() || !userEmail.trim()) {
+      alert("사용자 이름과 이메일을 모두 입력해주세요.");
+      return;
+    }
+
+    if (!selectedTeam) {
+      alert("팀을 선택해주세요.");
+      return;
+    }
+
+    const userKey = userEmail.split("@")[0]; // 이메일에서 @ 기준으로 왼쪽 추출
+
+    setTeamData((prevData) => {
+      // 중복 사용자 확인
+      if (prevData[selectedTeam].users[userKey]) {
+        alert("이미 존재하는 사용자입니다.");
+        return prevData;
+      }
+
+      // 새로운 사용자 추가
+      return {
+        ...prevData, // 기존 데이터를 유지
+        [selectedTeam]: {
+          ...prevData[selectedTeam], // 선택된 팀 데이터를 유지
+          users: {
+            ...prevData[selectedTeam].users, // 기존 사용자 데이터 유지
+            [userKey]: { email: userEmail, name: userName }, // 새로운 사용자 추가
+          },
+        },
+      };
+    });
+
+    handleCloseModal();
+  };
+
+  const renderModalContent = () => {
+    if (modalType === "team") {
+      return (
+        <div>
+          <h2 className="mb-4 text-lg font-semibold text-blue-400">팀 추가</h2>
+          <input
+            type="text"
+            placeholder="새 팀 이름을 입력하세요"
+            className="w-full p-2 mb-4 text-gray-900 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            ref={assignTeam}
+          />
+          <button
+            onClick={handleAddTeam}
+            className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            추가
+          </button>
+        </div>
+      );
+    } else if (modalType === "user") {
+      return (
+        <div>
+          <h2 className="mb-4 text-lg font-semibold text-blue-400">
+            사용자 추가
+          </h2>
+          <input
+            type="text"
+            placeholder="새 사용자 이름을 입력하세요"
+            className="w-full p-2 mb-4 text-gray-900 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            ref={assignUserName}
+          />
+          <input
+            type="email"
+            placeholder="이메일을 입력하세요"
+            className="w-full p-2 mb-4 text-gray-900 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            ref={assignUserEmail}
+          />
+          <button
+            onClick={handleAddUser}
+            className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            추가
+          </button>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <div className="w-full max-w-md p-8 bg-gray-800 rounded-lg shadow-xl">
+        <h1 className="mb-6 text-3xl font-semibold text-center text-blue-400">
+          주간 보고 프로젝트
+        </h1>
+
+        <div className="flex items-center mb-4">
+          <div className="flex-grow">
+            <label
+              htmlFor="team"
+              className="block text-lg font-medium text-gray-300"
+            >
+              팀 선택
+            </label>
+            <select
+              id="team"
+              name="team"
+              value={selectedTeam}
+              onChange={handleTeamChange}
+              className="w-full p-2 mt-2 text-white bg-gray-700 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              <option value="">팀을 선택하세요</option>
+              {Object.keys(teamData).map((teamKey) => (
+                <option key={teamKey} value={teamKey}>
+                  {teamData[teamKey].name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            onClick={() => handleOpenModal("team")}
+            className="ml-4 mt-8 p-2 h-[42px] bg-gray-700 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-400 flex items-center justify-center"
+          >
+            +
+          </button>
+        </div>
+
+        <div className="flex items-center mb-6">
+          <div className="flex-grow">
+            <label
+              htmlFor="name"
+              className="block text-lg font-medium text-gray-300"
+            >
+              이름 선택
+            </label>
+            <select
+              id="name"
+              name="name"
+              value={selectedName}
+              onChange={handleNameChange}
+              className="w-full p-2 mt-2 text-white bg-gray-700 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              disabled={!selectedTeam}
+            >
+              <option value="">이름을 선택하세요</option>
+              {userOptions.map((user) => (
+                <option key={user.email} id={user.email} value={user.name}>
+                  {user.name} ({user.email})
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            onClick={() => handleOpenModal("user")}
+            className="ml-4 mt-8 p-2 h-[42px] bg-gray-700 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-400 flex items-center justify-center"
+            disabled={!selectedTeam}
+          >
+            +
+          </button>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            onClick={handleSubmit}
+            className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+          >
+            확인
+          </button>
+        </div>
+      </div>
+      <Modal open={modalIsOpen} onClose={handleCloseModal}>
+        {renderModalContent()}
+      </Modal>
+    </div>
+  );
+}
