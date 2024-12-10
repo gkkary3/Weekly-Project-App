4ed783220e25f5e5a6cbede30fa285fe4f51bd7d
gkkary3 <nambawon1@naver.com>
2024-12-10 23:03:13 +0900

    commit in

--- backend/app.js
+++ backend/app.js
@@ -218,4 +218,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(3000, () => console.log("Server is running on ports 3000"));
