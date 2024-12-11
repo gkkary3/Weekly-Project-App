83a070cfb55a297ca761d2ff1621cbdf660b1c2f
Parksangmin <psangmin3@zaisoft.co.kr>
2024-12-11 10:53:07 +0900

    frontend build add

--- backend/app.js
+++ backend/app.js
@@ -211,10 +211,7 @@ app.delete(
 );
 
 // 정적 파일 서빙 (필요 시)
-app.use(
-  "/",
-  express.static(path.join(__dirname, "../frontend/build/index.html"))
-);
+app.use("/", express.static(path.join(__dirname, "../frontend/build")));
 
 // 모든 다른 요청을 index.html로 리다이렉트
 app.get("*", (req, res) => {
