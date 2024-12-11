89d4f6c09fe4f6283b6982b05e5b1095ef704c95
Parksangmin <psangmin3@zaisoft.co.kr>
2024-12-11 15:56:39 +0900

    App.js commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
   const handleTeamSubmit = (submitTeam) => {
     setSubmitTeamData(submitTeam);
     setTeamIsSubmit(true);
+    setTeamSubmitData(true);
   };
 
   const handleHeight = (headerHeight) => {
