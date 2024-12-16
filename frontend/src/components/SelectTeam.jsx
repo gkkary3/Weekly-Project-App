a921720360f4d01a574843d149fdfb85c51fe0ca
gkkary3 <nambawon1@naver.com>
2024-12-16 12:06:48 +0900

    user-report commit

--- frontend/src/components/SelectTeam.jsx
+++ frontend/src/components/SelectTeam.jsx
@@ -218,8 +218,8 @@ export default function SelectTeam({ handleTeamSubmit, handlefetchResult }) {
           if (prev.id === selectedUser.id) {
             return {
               ...prev,
-              userName: userName,
-              userEmail: userEmail,
+              userName: assignUserName.current.value,
+              userEmail: assignUserEmail.current.value,
             };
           }
           return prev;
